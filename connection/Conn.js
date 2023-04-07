e2cf659663e61d2f9c9638f8dfaa30950042ba0d
vm2124 <vm21242124@gmail.com>
2023-04-07 10:20:50 +0530

    connection

--- connection/Conn.js
+++ connection/Conn.js
@@ -5,8 +5,7 @@ export const connection = async () => {
     const conn = await mongoose.connect(process.env.URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
-    console.log("connected to mongodb");
+    }).then((console.log("connected to db"))).catch((e)=>console.log(e))
   } catch (error) {
     console.log(error);
   }
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connection } from "./connection/Conn.js";
 import AuthRoute from "./Routes/AuthRoute.js";
 import bodyParser from 'body-parser'
+import mongoose from "mongoose";
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,7 +11,15 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 dotenv.config();
-connection();
+
+try {
+   mongoose.connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then((console.log("connected to db"))).catch((e)=>console.log(e))
+} catch (error) {
+  console.log(error);
+}
 
 app.listen(process.env.PORT, (err) => {
   if (!err) {
